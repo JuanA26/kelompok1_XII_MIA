9f46ccb63b97669f127d983714cb56345c81e83c
JuanA26 <92071328+JuanA26@users.noreply.github.com>
2022-01-27 13:49:31 +0800

    Add files via upload

---
+++ project-1/Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+
+    Private Sub PictureBox1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles PictureBox1.Click
+
+    End Sub
+
+    Private Sub Label1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub PictureBox3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles PictureBox3.Click
+
+    End Sub
+End Class
